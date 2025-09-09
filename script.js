d844df7dd3a51ea21fc7497e39e638553709ee75
OkIbrahim <155417872+OkIbrahim@users.noreply.github.com>
2025-09-09 20:53:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -71,14 +71,15 @@ allLinks.forEach(function (link) {
     }
 
     // Close mobile navigation
-    if (link.classList.contains("main-nav-link"))
-      headerEl.classList.toggle("nav-open");
-
     if (link.classList.contains("main-nav-cta"))
+      headerEl.classList.toggle("nav-open");
+    
+    if (link.classList.contains("main-nav-link"))
       headerEl.classList.toggle("nav-open");
   });
 
   
 });
+
 
 
